b6f01f29c8d6b29495f02ecf875590265a1a369d
Thang Le <jakethecat21@gmail.com>
2023-03-18 17:10:55 +0700

    rm frontend

--- frontend/src/reducers.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import container from './components/containers/reducer';
-
-const rootReducer = combineReducers({ container });
-
-export type RootState = ReturnType<typeof rootReducer>;
-
-export default rootReducer;
